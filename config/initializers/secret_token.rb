bb858e5da3ae23aedb639a97dd6b76a6ed484551
Andrey Chernih <andrey.chernih@gmail.com>
2013-01-29 10:52:29 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IosToolbox::Application.config.secret_token = '35dee5460d946d7ce61d0ed66c3640cc8b20cca1762ebb74f0f419cd429ed2ef9eec805223f86f1a446b9b6468912d0716e9086d927052524b3601c0ff213e65'
