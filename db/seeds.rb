885cf4aac8594982fd858c9c96a70d6212c40fca
Andrey Chernih <andrey.chernih@gmail.com>
2013-02-05 15:15:50 +0400

    Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,5 @@ cat_networking = Category.create! name: 'Networking'
 cat_graphics   = Category.create! name: 'Graphics'
 
 project_asi    = cat_networking.projects.create! name: 'ASIHTTPRequest', url: 'http://allseeing-i.com/ASIHTTPRequest/'
+project_af     = cat_networking.projects.create! name: 'AFNetworking',   url: 'https://github.com/AFNetworking/AFNetworking'
+project_rest   = cat_networking.projects.create! name: 'RestKit',        url: 'http://restkit.org/'
