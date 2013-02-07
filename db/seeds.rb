0f91ae05e801db707135432953d75baf990a6349
Andrey Chernih <andrey.chernih@gmail.com>
2013-02-07 15:08:14 +0400

    Add GitHub paths to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@ Category.destroy_all
   Category.create! name: category_name
 end
 
-[{category: 'Networking', name: 'ASIHTTPRequest', url: 'http://allseeing-i.com/ASIHTTPRequest/'},
- {category: 'Networking', name: 'AFNetworking', url: 'https://github.com/AFNetworking/AFNetworking'},
- {category: 'Networking', name: 'RestKit', url: 'http://restkit.org/'}].each do |project_attributes|
+[{category: 'Networking', name: 'ASIHTTPRequest', url: 'http://allseeing-i.com/ASIHTTPRequest/',       github_path: 'pokeb/asi-http-request'},
+ {category: 'Networking', name: 'AFNetworking',   url: 'https://github.com/AFNetworking/AFNetworking', github_path: 'AFNetworking/AFNetworking'},
+ {category: 'Networking', name: 'RestKit',        url: 'http://restkit.org/',                          github_path: 'RestKit/RestKit'}].each do |project_attributes|
   Project.create! do |p|
     p.category    = Category.find_by_name! project_attributes[:category]
     p.name        = project_attributes[:name]
